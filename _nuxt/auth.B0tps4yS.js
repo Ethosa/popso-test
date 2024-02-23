42559f9cc8ee1871be4572d066e5692e5cd1f889
Ethosa <Ethosa@users.noreply.github.com>
2024-02-23 03:40:05 +0000

    deploy: 11eff3692ca6dbf57b47e81b678dc66728515780

---
+++ _nuxt/auth.B0tps4yS.js
@@ -0,0 +1,1 @@
+import{a4 as e}from"./entry.D1z3GYlH.js";const s=e("auth",{state:()=>({isAuthorized:!1,userId:0,firstName:"",lastName:""})});export{s as u};
--- _nuxt/auth.Cmy0ZcG6.js
+++
@@ -1,1 +0,0 @@
-import{a3 as a,q as e}from"./entry.CTGQmJw_.js";import{u as o}from"./auth.BcBFhFaH.js";const f=a((t,u)=>{if(!o().isAuthorized&&t.fullPath!=="/")return e("/")});export{f as default};
